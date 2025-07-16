5301b9afd4c096a748cca523c40edb5dec5677c6
sayakghosh <sayakghosh2004sg@gmail.com>
2025-07-16 12:15:01 +0530

    fixed the build error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://nxzdoenrrhbrozcqpkqx.supabase.co";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54emRvZW5ycmhicm96Y3Fwa3F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MDk0MDMsImV4cCI6MjA2NzQ4NTQwM30.u9809vQXUFrc7lCvXZnz9opJykVn9gW41b36Y2tyMFg";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
